d2f5a2e7d07df6b1803a62e97aceb53d2a1d380e
Parthiban Loganathan <pl2487@columbia.edu>
2015-07-14 22:02:21 -0700

    Initial commit

---
+++ tea.js
@@ -0,0 +1,122 @@
+// Client
+if (Meteor.isClient) {
+  Meteor.subscribe("people");
+
+  Template.people.helpers({
+    'people': function() {
+      return PeopleCollection.find().fetch();
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Template.profile.helpers({
+    'insert': function(name,
+        display_name,
+        school,
+        about,
+        uni,
+        twitter,
+        facebook,
+        linkedin) {
+      Meteor.call("insertUser",
+          Meteor.userId(),
+          Meteor.user().username,
+          name,
+          display_name,
+          school,
+          about,
+          uni,
+          twitter,
+          facebook,
+          linkedin);
+    },
+        'set-availability': function() {
+          Meteor.call("setAvailability", 
+              Meteor.userId(),
+              Meteor.user().username,        
+              monday_times,
+              tuesday_times,
+              wednesday_times,
+              thursday_times,
+              friday_times,
+              saturday_times,
+              sunday_times);
+        }
+  });
+}
+
+// Common
+PeopleCollection = new Mongo.Collection("people");
+AvailabilityCollection = new Mongo.Collection("availability");
+
+Meteor.methods({
+  insertUser: function (id,
+                  username,
+                  name,
+                  display_name,
+                  school,
+                  about,
+                  uni,
+                  twitter,
+                  facebook,
+                  linkedin) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    PeopleCollection.insert({
+      owner: id, 
+      username: username,        
+      name: name,
+      display_name: display_name,
+      school: school,
+      about: about,
+      uni: uni,
+      twitter: twitter,
+      facebook: facebook,
+      linkedin: linkedin
+    });
+  },
+  deleteUser: function (id) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    PeopleCollection.remove({owner: id});
+  },
+  setAvailability: function (id,
+                       username,
+                       monday_times,
+                       tuesday_times,
+                       wednesday_times,
+                       thursday_times,
+                       friday_times,
+                       saturday_times,
+                       sunday_times) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    AvailabilityCollection.update({
+      owner: id,
+      username: username,        
+      monday: monday_times,
+      tuesday: tuesday_times,
+      wednesday: wednesday_times,
+      thursday: thursday_times,
+      friday: friday_times,
+      saturday: saturday_times,
+      sunday: sunday_times,
+    });
+  }
+});
+
+// Server
+if (Meteor.isServer) {
+  Meteor.publish("people", function () {
+    return PeopleCollection.find();
+  });
+}
