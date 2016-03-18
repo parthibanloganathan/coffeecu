cef1be1fff4b5df1ac8674707c464150a814618a
Parthiban Loganathan <pl2487@columbia.edu>
2016-03-18 17:49:56 -0400

    fix

--- imgcpy.sh
+++ imgcpy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Copying images from last to current bundle..."
-cp -r /opt/coffeecu/last/bundle/.uploads /opt/coffeecu/current/bundle/.uploads
+cp -r /opt/coffeecu/last/bundle/.uploads/* /opt/coffeecu/current/bundle/.uploads
 echo "Done"
