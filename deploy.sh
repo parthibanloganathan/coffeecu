c2bbc479fa14f53cba7ad2b3496a56f9e0312449
Parthiban Loganathan <pl2487@columbia.edu>
2016-04-10 10:45:42 -0400

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ mupx deploy
 echo "Copying images from last to current bundle..."
 cp -r /opt/coffeecu/last/bundle/.uploads/* /opt/coffeecu/current/bundle/.uploads
 echo "Creating image backups"
-cp -r /opt/coffeecu/current/bundle/.uploads/* /dev/backups
+cp -r /opt/coffeecu/current/bundle/.uploads/* ~/dev/backups
 echo "Done"
