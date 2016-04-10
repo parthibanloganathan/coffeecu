4e0f260fc90fc94b1f8c0f02fabd10c9514fd26e
Parthiban Loganathan <pl2487@columbia.edu>
2016-04-10 09:51:36 -0400

    Change script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mupx deploy
 echo "Copying images from last to current bundle..."
 cp -r /opt/coffeecu/last/bundle/.uploads/* /opt/coffeecu/current/bundle/.uploads
+echo "Creating image backups"
+cp -r /opt/coffeecu/current/bundle/.uploads/* /dev/backups
 echo "Done"
